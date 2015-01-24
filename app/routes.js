3f301efa9deb88e47dee507c41b4c38a41db7c54
Ramzie <tom.ras86@gmail.com>
2015-01-24 01:39:47 -0800

    fixed routes.js bugs

--- app/routes.js
+++ app/routes.js
@@ -1,12 +1,12 @@
 module.exports = function(app){
 
 
+	app.get( '/index/', function(){
+	console.log( 'index');
+	});
+	
 	app.get( '*', function(){
 	console.log( 'all routes');
-	};
-	
-	app.get( '/index/', funcion(){
-	console.log( 'index');
 	});
 
 }
