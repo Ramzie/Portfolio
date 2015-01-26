76084b971fa464a481d1a36b116f2291b235d4d1
Ramzie <tom.ras86@gmail.com>
2015-01-25 23:24:54 -0800

    Added an api route to routes.js

--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,9 @@
 module.exports = function(app){
 
-
+	app.get( '/api/user_timeline', function(req, res){
+	res.send('you hit my api route');
+	});
+	
 	app.get( '*', function(req, res){
 	res.sendfile( './public/index.html');
 	});
