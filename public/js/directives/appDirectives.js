8092a44a2b67d8b7195ecb8f3ca4e682e46911f8
Ramzie <tom.ras86@gmail.com>
2015-02-07 02:28:35 -0800

    added pictures folder, worked on gallery directive

--- public/js/directives/appDirectives.js
+++ public/js/directives/appDirectives.js
@@ -3,16 +3,53 @@ angular.module('appDirectives', [])
 	.directive('galleryDirective', function(){
 		return{
 			restrict: 'A',
-			link: function(scope, element, attrs){
-				//console.log('scope',scope);
-				//console.log('element',element[0]);
-				//console.log('element',element);
-				//console.log('pcontent',element[0].children[0].textContent);
-				//console.log('attrs', attrs);
-				//element[0].children[0].textContent='vegas is a turkey';
-				console.log('galleryDirective',attrs.galleryDirective);
-				console.log('galleryDirective type', typeof attrs.galleryDirective );
-				console.log('galleryDirective JSON',JSON.parse(attrs.galleryDirective).type );
+			link: function(scope){
+				scope.pictures =[ {'id':'1','imageUrl':'pictures/1.jpg'},
+												{'id':'2','imageUrl':'pictures/2.jpg'},
+												{'id':'3','imageUrl':'pictures/3.jpg'},
+												{'id':'4','imageUrl':'pictures/4.jpg'},
+												{'id':'5','imageUrl':'pictures/5.jpg'}
+											];
+			},
+			template: function(){
+				
+				return [
+				'<div ng-repeat="picture in pictures" ng-click="foo = picture.imageUrl">',
+					'<img class="thumb" src="{{picture.imageUrl}}">',
+				'</div>'
+				].join("")
 			}
+
 		}
 	});
+
+
+
+
+
+
+
+
+	/*return{
+					<div ng-repeat='picture in pictures'>
+						<img ng-class"thumbnail" src"{{/pictures/picture.id}}">
+					</div>
+				}*/
+				/*link: function(scope, element, attrs){
+				console.log('scope',scope);
+				console.log('element',element[0]);
+				console.log('element',element);
+				console.log('pcontent',element[0].children[0].textContent);
+				console.log('attrs', attrs);
+				//element[0].children[0].textContent='vegas is a turkey';
+				//console.log('galleryDirective',attrs.galleryDirective);
+				//console.log('galleryDirective type', typeof attrs.galleryDirective );
+				//console.log('galleryDirective JSON',JSON.parse(attrs.galleryDirective).type );
+				}*/
+
+
+
+						/*restrict: 'A',
+		//template: function(){
+				var pictures = [
+								]*/
