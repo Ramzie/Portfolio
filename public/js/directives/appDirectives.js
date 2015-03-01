c17be98c5dec097e98d7fee5146bcfcb50989f6e
Ramzie <tom.ras86@gmail.com>
2015-02-28 22:34:40 -0800

    more clean up

--- public/js/directives/appDirectives.js
+++ public/js/directives/appDirectives.js
@@ -2,37 +2,25 @@
 angular.module('appDirectives', [])
 	.directive('galleryDirective', function(flickrService, lodash){
 		return{
-			scope: {
-				flickrImg: '='
-			},
+			
 			restrict: 'A',
 			link: function(scope){
-				console.log('scope',scope);
 				scope.selected = 4;
 				flickrService.get()
 				.success(function(data) {
-    			// this callback will be called asynchronously
-    			// when the response is available
-    			//console.log(data);
-    			var goodJson = JSON.parse(data.slice(14, data.length-1));
-    			//console.log(goodJson.photos.photo);	
-    			scope.pictures = lodash.reduce(goodJson.photos.photo, function(arrayImgs, img, id){
+    			// this parses the flickr API data
+    			var parsedData = JSON.parse(data.slice(14, data.length-1));
+    			//uses lodash to iterate through parsedData to create an image array
+    			scope.pictures = lodash.reduce(parsedData.photos.photo, function(arrayImgs, img, id){
                     var imgLink = 'https://farm' + img.farm + '.static.flickr.com/' + img.server + '/' + img.id + '_' + img.secret + '_z.jpg';
     				var imgObj = {'id':id,'imageUrl':imgLink};
                     arrayImgs[id]=imgObj;
                     return arrayImgs;
     			}, []);
   			})
-
-				/*scope.pictures =[ {'id':'1','imageUrl':'pictures/1.jpg'},
-												{'id':'2','imageUrl':'pictures/2.jpg'},
-												{'id':'3','imageUrl':'pictures/3.jpg'},
-												{'id':'4','imageUrl':'pictures/4.jpg'},
-												{'id':'5','imageUrl':'pictures/5.jpg'}
-											];*/
 			},
 			template: function(){
-				
+				//displays images from flickr
 				return [
 				'<div  ng-repeat="picture in pictures" ng-click="$parent.selected=$index">',
 				'<img src="{{picture.imageUrl}}" ng-class="{embiggen: selected == $index}">',
@@ -41,7 +29,3 @@ angular.module('appDirectives', [])
 			}
 		}
 	});
-
-	/*'<div ng-repeat="picture in pictures" ng-click="foo = !foo">',
-				'<img src="{{picture.imageUrl}}" ng-class="{embiggen: foo}">',
-				'</div>'*/
--- public/js/services/appServices.js
+++ public/js/services/appServices.js
@@ -1,7 +1,7 @@
-angular.module('appServices', []).factory('flickrService', function($http, lodash){
+angular.module('appServices', []).factory('flickrService', function($http){
 	return{
+		//keeping my api key private by using Node.JS to serve the flickr API url.
 		get : function(){
-			//var flickrVar = 'https://api.flickr.com/services/rest/?method=flickr.people.getPublicPhotos&api_key=db74013332d5c9c32faca757c080e6be&user_id=7718810@N02&per_page=10&format=json';
            return $http.get('/api/flickr_data')
 		}
 	}
